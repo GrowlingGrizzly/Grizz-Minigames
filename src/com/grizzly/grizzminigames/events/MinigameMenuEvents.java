51fa733d69f742369a91f4f3f0724ad0d226bbb1
GrowlingGrizzly <growlinggrizzlybear@gmail.com>
2021-06-02 09:54:53 -0400

    Suppressed non-needed warnings

--- src/com/grizzly/grizzminigames/events/MinigameMenuEvents.java
+++ src/com/grizzly/grizzminigames/events/MinigameMenuEvents.java
@@ -18,6 +18,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
+@SuppressWarnings({"unused", "ConstantConditions"})
 public class MinigameMenuEvents implements Listener {
 
     Grizz plugin = Grizz.pluginMain;
